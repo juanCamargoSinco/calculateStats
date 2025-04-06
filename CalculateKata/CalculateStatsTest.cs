94061c6f4210a27dec1189207c0fa0aeed627f5e
Juan Manuel Camargo Segura <juan.camargo@sinco.co>
2025-04-06 14:07:07 -0500

    🟦 Debe_calcular_valor_minimo_de_secuencia

--- CalculateKata/CalculateStatsTest.cs
+++ CalculateKata/CalculateStatsTest.cs
@@ -11,29 +11,19 @@ namespace CalculateKata
             Assert.Throws<Exception>(() => ProcesadorSecuencias.ProcesarSecuencia(secuenciaNumeros));
         }
 
-        [Fact]
-        public void Debe_calcular_valor_minimo_de_secuencia()
-        {
-            IEnumerable<int> secuenciaNumeros = [1];
 
-            Assert.Equal(1, ProcesadorSecuencias.CalcularValorMinimo(secuenciaNumeros));
+        [Theory]
+        [
+            InlineData(new [] { 1 }, 1),
+            InlineData(new[] { 5, 2 }, 2),
+            InlineData(new[] { 6, 9, 15, -2, 92, 0 }, -2),
+
+        ]
+        public void Debe_calcular_valor_minimo_de_secuencia(IEnumerable<int> secuenciaNumeros, int valorMinimoSecuencia)
+        {
+            Assert.Equal(valorMinimoSecuencia, ProcesadorSecuencias.CalcularValorMinimo(secuenciaNumeros));
         }
 
-        [Fact]
-        public void Debe_calcular_valor_minimo_de_secuencia_cuando_secuencia_tiene_dos_elementos()
-        {
-            IEnumerable<int> secuenciaNumeros = [5, 2];
-
-            Assert.Equal(2, ProcesadorSecuencias.CalcularValorMinimo(secuenciaNumeros));
-        }
-
-        [Fact]
-        public void Debe_calcular_valor_minimo_de_secuencia_cuando_secuencia_tiene_seis_elementos()
-        {
-            IEnumerable<int> secuenciaNumeros = [6, 9, 15, -2, 92, 0];
-
-            Assert.Equal(-2, ProcesadorSecuencias.CalcularValorMinimo(secuenciaNumeros));
-        }
     }
 
     public class ProcesadorSecuencias
