43960f8876e1560a1650001b24068d94c3ccc4ca
Juan Manuel Camargo Segura <juan.camargo@sinco.co>
2025-04-06 13:26:16 -0500

    🟩 Devolver error al procesar arreglo vacio

--- CalculateKata/CalculateStatsTest.cs
+++ CalculateKata/CalculateStatsTest.cs
@@ -6,9 +6,18 @@ namespace CalculateKata
         public void Debe_devolver_error_al_procesar_arreglo_vacio()
         {
             IEnumerable<int> secuenciaNumeros = Array.Empty<int>();
+            
 
             Assert.Throws<Exception>(() => ProcesadorSecuencias.ProcesarSecuencia(secuenciaNumeros));
         }
 
+    }
+
+    public class ProcesadorSecuencias
+    {
+        public static void ProcesarSecuencia(IEnumerable<int> secuenciaNumeros)
+        {
+            throw new Exception();
+        }
     }
 }
